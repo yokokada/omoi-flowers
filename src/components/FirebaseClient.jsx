a507a08ef1d4cd77ca8c5ecefb1345ff205050c2
yokokada <yokoumasaka@gmail.com>
2023-09-03 22:10:40 +0900

    環境設定関連変更

--- src/components/FirebaseClient.jsx
+++ src/components/FirebaseClient.jsx
@@ -5,12 +5,12 @@ import { getFirestore } from 'firebase/firestore';
 
 const config = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DB_URL,
+    authDomain: "omoi-flowers.firebaseapp.com",
     projectId: "omoi-flowers",
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    storageBucket: "omoi-flowers.appspot.com",
     messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_SENDER_ID,
     appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID 
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(config);
