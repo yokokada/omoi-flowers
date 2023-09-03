1cff66eaee487fc92dfc41d206c84d2675b729fb
yokokada <yokoumasaka@gmail.com>
2023-09-03 15:39:37 +0900

    id欄追加

--- src/components/ClickHistory.jsx
+++ src/components/ClickHistory.jsx
@@ -2,22 +2,26 @@ import React from 'react';
 
 const ClickHistory = ({ history }) => {
     return (
-        <table className="history-table">
-            <thead>
-                <tr>
-                    <th>クリック数</th>
-                    <th>時間</th>
-                </tr>
-            </thead>
-            <tbody>
-                {[...history].reverse().map((entry, index) => (
-                <tr key={index}>
-                        <td>{entry.count}</td>
-                        <td>{entry.time}</td>
+        <div style={{ maxHeight: '300px', overflowY: 'auto' }}>
+            <table className="history-table">
+                <thead>
+                    <tr>
+                        <th>クリック数</th>
+                        <th>時間</th>
+                        <th>ID</th> {/* 新しいセルを追加 */}
                     </tr>
-                ))}
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    {[...history].reverse().map((entry, index) => (
+                        <tr key={index}>
+                            <td>{entry.count}</td>
+                            <td>{entry.time}</td>
+                            <td>{entry.id}</td> {/* IDを表示 */}
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
     );
 };
 
