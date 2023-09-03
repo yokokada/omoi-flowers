364963f4d325b51e19c22d5e19729b4f694adc30
yokokada <yokoumasaka@gmail.com>
2023-09-03 18:18:59 +0900

    環境変数設定

--- src/components/FirebaseClient.jsx
+++ src/components/FirebaseClient.jsx
@@ -9,7 +9,7 @@ if (!firebase.apps.length) {
         apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
         authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
         databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DB_URL,
-        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+        projectId: "omoi-flowers",
         storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
         messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_SENDER_ID,
         appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID 
